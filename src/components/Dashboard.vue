2345d750dd0f07a112803a037edbbf59bf585542
Nhanvt <nhanvt@mor.com.vn>
2023-03-20 17:33:13 +0700

    config firebase

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="dashboard">
-    <h3>dashboard</h3>
+    <ul class="collection with-header">
+      <li class="collection-header">
+        <h4>Employees</h4>
+      </li>
+      <li
+        v-for="employee in employees"
+        v-bind:key="employee.id"
+        class="collection-item"
+      >
+        <div class="chip">{{ employee.dept }}</div>
+        {{ employee.name }}
+
+        <router-link
+          class="secondary-content"
+          v-bind:to="{
+            name: 'view-employee',
+            params: { employee_id: employee.employee_id },
+          }"
+        >
+          <i class="fa fa-eye"></i>
+        </router-link>
+      </li>
+    </ul>
     <div class="fixed-action-btn">
       <router-link to="/new" class="btn-floating btn-large red">
         <i class="fa fa-plus"></i>
@@ -15,22 +37,21 @@ export default {
   name: "dashboard",
   data() {
     return {
-      employee: [],
+      employees: [],
     };
   },
   async created() {
     const db = getFirestore(app);
-    console.log(db);
     const citiesCol = collection(db, "employees");
-    console.log("==1", citiesCol);
     const citySnapshot = await getDocs(citiesCol);
-    console.log("==2", citySnapshot);
     citySnapshot.forEach((doc) => {
-      const data = {
+      this.employees.push({
         id: doc.id,
         employee_id: doc.data().employee_id,
-      };
-      console.log(data);
+        name: doc.data().name,
+        dept: doc.data().dept,
+        position: doc.data().position,
+      });
     });
   },
 };
--- src/components/ViewEmployee.vue
+++ src/components/ViewEmployee.vue
@@ -5,10 +5,22 @@
 </template>
 
 <script>
+import app from "./firebaseInit";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
 export default {
   name: "view-employee",
   data() {
-    return {};
+    return {
+      employee_id: null,
+      name: null,
+      dept: null,
+      position: null,
+    };
+  },
+  async created() {
+    const db = getFirestore(app);
+    const citiesCol = collection(db, "employees");
   },
 };
 </script>
